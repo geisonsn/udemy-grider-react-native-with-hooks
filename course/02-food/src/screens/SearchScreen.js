87c21e2cf1991716442f937f93903f4584df912c
Geison Santos <geison.sn@gmail.com>
2020-04-26 21:58:17 +0000

    Refactoring to separate business logic and view in SearchScreen component

--- course/02-food/src/screens/SearchScreen.js
+++ course/02-food/src/screens/SearchScreen.js
@@ -1,29 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
-const SearchSCreen = () => {
+const SearchScreen = () => {
 
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async searchTerm => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-  
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
@@ -40,4 +23,4 @@ const SearchSCreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default SearchSCreen;
+export default SearchScreen;

43321ed7e96da564e2bd15b7e337c59fe815e78f
Geison Santos <geison.sn@gmail.com>
2020-05-01 23:15:51 +0000

    Sharing an object through Context

--- course/03-blog/src/context/BlogContext.js
+++ course/03-blog/src/context/BlogContext.js
@@ -3,8 +3,11 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+
+  const blogPosts = [{title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
+
   return (
-    <BlogContext.Provider value={["Sharing data with child component"]}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
   );
 };
 

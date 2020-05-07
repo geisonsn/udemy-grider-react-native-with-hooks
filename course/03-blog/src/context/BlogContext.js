bf17df45582704987033f37cef2e8e48f609eae4
Geison Santos <geison.sn@gmail.com>
2020-05-07 18:25:03 +0000

    Add feature to edit posts

--- course/03-blog/src/context/BlogContext.js
+++ course/03-blog/src/context/BlogContext.js
@@ -3,8 +3,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
-        return state.filter(blogPost => blogPost.id !==  action.payload);
+      return state.filter(blogPost => blogPost.id !==  action.payload);
     case 'add_blogpost':
       return [
         ...state, 
@@ -32,8 +36,15 @@ const deleteBlogPost = dispatch => {
   };
 };
 
+const editBlogPost = dispatch => {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content }});
+    callback();
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  []
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'Teste', content: 'Some content'}]
 );
--- course/03-blog/src/screens/ShowScreen.js
+++ course/03-blog/src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, {useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
 
@@ -14,6 +15,17 @@ const ShowScreen = ({ navigation }) => {
     </View>
   );  
 };
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => {
+        return <TouchableOpacity onPress={() => { navigation.navigate('Edit', { id: navigation.getParam('id')})}}>
+            <EvilIcons name="pencil" size={35} />
+        </TouchableOpacity>
+    }
+  };
+};
+
 
 const styles = StyleSheet.create({
 });

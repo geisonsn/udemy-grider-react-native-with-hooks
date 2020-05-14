f71888beb91b79f186ae0f437dc3c91355673b59
Geison Santos <geison.sn@gmail.com>
2020-05-14 20:00:57 +0000

    Delete a new post using local API

--- course/03-blog/src/context/BlogContext.js
+++ course/03-blog/src/context/BlogContext.js
@@ -42,7 +42,8 @@ const addBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-  return (id) => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
